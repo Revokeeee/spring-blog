00f2bf197e3de801ae930733bcbab7ecf9e979d8
Revokeeee <maksim_25_03@ukr.net>
2025-06-04 13:40:12 +0300

    Added Recorded test

---
+++ src/test/java/com/example/blog/RecordedTestCase.java
@@ -0,0 +1,91 @@
+package com.example.blog;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+
+import java.time.Duration;
+
+public class RecordedTestCase {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+    WebDriverManager.chromedriver().setup(); // автоматично завантажує правильний драйвер
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testUntitledTestCase() throws Exception {
+    driver.get("http://localhost:8080/");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Revoke'])[1]/following::p[1]")).click();
+    driver.findElement(By.linkText("Увійдіть")).click();
+    driver.findElement(By.name("username")).click();
+    driver.findElement(By.name("username")).clear();
+    driver.findElement(By.name("username")).sendKeys("Revoke");
+    driver.findElement(By.name("password")).click();
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("123123123");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='First Post'])[1]/following::p[1]")).click();
+    driver.findElement(By.name("content")).click();
+    driver.findElement(By.name("content")).clear();
+    driver.findElement(By.name("content")).sendKeys("Test comment");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+  }
+
+  @After()
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
